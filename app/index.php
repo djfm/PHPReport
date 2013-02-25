83709afcf9f4a61da664ef047996458e802886e8
fram <fm.de.jouvencel@gmail.com>
2013-02-25 13:51:22 +0100

    evaluator

--- app/index.php
+++ app/index.php
@@ -1,5 +1,5 @@
 <?php
-
+ini_set('display_errors','on');
 require_once __DIR__.'/../vendor/autoload.php';
 
 require_once __DIR__.'/lib/Report.php';
@@ -7,7 +7,7 @@ require_once __DIR__.'/lib/Report.php';
 $app = new Silex\Application();
 
 $app['debug'] = true;
-ini_set('display_errors','on');
+
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
@@ -15,12 +15,13 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $app->get('/', function() use ($app){
 
+	
 	$report = new Report;
-	$report->load(__DIR__ . '/reports/report.xml');
+	$report->load(__DIR__ . '/reports/shops.xml');
 	$report->compute();
-	//echo $report->renderAsHTML();
+	echo $report->renderAsHTML();
 
-	$report->sendAsExcel();
+	//$report->sendAsExcel();
 
 	return $app['twig']->render('index.html.twig', array());
 })->bind('home');
